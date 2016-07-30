f0583d7584b36050ba5a8473b9ebaa5f8f6bd2cf
Arun Babu <arunbabu-madhavan>
2016-07-30 20:55:07 +0530

    Added AppComponent and base html

---
+++ app/app.component.ts
@@ -0,0 +1,10 @@
+import {Component} from 'angular2/core';
+
+@Component({
+selector:'poke-dex',
+templateUrl:'app/app.component.html',
+
+})
+export class AppComponent{
+    isPokeBallOpen:boolean=false;
+}
